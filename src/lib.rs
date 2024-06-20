d5ac1a8c363121b5abc6eb516abe719b6a601e06
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2024-06-20 03:45:27 -0400

    add path buff and folder vec function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,36 @@
-pub fn get_file_names_from_folder(folder_path: String) -> Vec<String> {
-    vec!["stub".to_string(); 2]
+use std::{
+    fs::read_dir,
+    path::{Path, PathBuf},
+    io,
+};
+
+// see reference implementations
+// https://www.thorsten-hans.com/weekly-rust-trivia-get-all-files-in-a-directory/
+pub fn get_filenames_from_folder(folder_path: &String) -> io::Result<Vec<String>> {
+    let result = read_dir(Path::new(folder_path))?;
+
+    let files = result
+        .filter_map(|fd| {
+            let p = fd.ok()?.path();
+            if p.is_file() {
+                p.file_name()?.to_str().map(|s| s.to_owned())
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    Ok(files)
+}
+
+pub fn get_pathbuffers_from_folder(folder_path: &String) -> io::Result<Vec<PathBuf>> {
+    let result = read_dir(Path::new(folder_path))?;
+
+    let files = result
+        .filter_map(|file| Some(file.ok()?.path()))
+        .collect();
+
+    Ok(files)
 }
 
 #[cfg(test)]
@@ -8,6 +39,7 @@ mod tests {
 
     #[test]
     fn it_works() {
+        // TODO: testing..
         assert_eq!(4, 4);
     }
 }
